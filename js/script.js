395e78b46107ae35a924a00dbd8267e374533e7f
Alesja <graviella@mail.ru>
2022-03-28 06:23:18 +0300

    add: add a block Services

--- js/script.js
+++ js/script.js
@@ -1,8 +1,64 @@
+// click on burger-menu
 let burger = document.querySelector('.btn-burger'),
     spanBurger = document.querySelector('.span-burger')
     nav = document.querySelector('.nav')
+    
 
 burger.addEventListener('click', () => {
     nav.classList.toggle('turn-left')
     spanBurger.classList.toggle('btn-burger-close')
+    let ariaLabelBurger = burger.getAttribute('aria-label')
+    if(ariaLabelBurger == 'Разгарнуць меню') {
+        burger.setAttribute('aria-label', 'Згарнуць меню')
+    }
+    else {
+        burger.setAttribute('aria-label', 'Разгарнуць меню')
+    }
+})
+
+// click on a tab in a block of services
+let tabs = document.querySelectorAll('.tabs-nav__btn')
+
+tabs.forEach(function(tab) {
+    tab.addEventListener('click', function() {
+        let activeTabDescription = document.querySelector('.tab-description-active'),
+            activeTarget = activeTabDescription.getAttribute('data-target'),
+            newActivePath = this.getAttribute('data-path')
+        if(activeTabDescription && activeTarget != newActivePath) {
+            activeTabDescription.classList.remove('tab-description-active')
+            document.querySelector(`[data-target = ${newActivePath}]`).classList.add('tab-description-active')
+        }
+    })
+})
+
+// add scrool for tab
+tabs.forEach(function(tab) {
+    tab.addEventListener('click', function() {
+        let activeTabDescription = document.querySelector('.tab-description-active')
+        if(activeTabDescription) {
+            location.href = '#tab'
+        }
+    })
+})
+
+// click on a arrow in tabs
+let arrowPrev = document.querySelector('.tab-prev'),
+    arrowNext = document.querySelector('.tab-next')
+
+arrowPrev.addEventListener('click', () => {
+    activeTab = document.querySelector('.tab-description-active')
+    prevTab = activeTab.previousElementSibling
+    if(prevTab && prevTab.classList.contains('tab-description')) {
+        activeTab.classList.remove('tab-description-active')
+        prevTab.classList.add('tab-description-active')
+    }
+})
+
+arrowNext.addEventListener('click', () => {
+    activeTab = document.querySelector('.tab-description-active')
+    nextTab = activeTab.nextElementSibling
+    if(nextTab && nextTab.classList.contains('tab-description')) {
+        activeTab.classList.remove('tab-description-active')
+        nextTab.classList.add('tab-description-active')
+    }
 })
